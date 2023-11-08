9a334b6f6869ba9bcf7fd2a8dd4122cb9b203ae3
CHENNAOUI Khadija <Khadija.CHENNAOUI@cegedim.com>
2023-11-08 14:50:08 +0100

    create repository

---
+++ src/tasks/tasks.repository.ts
@@ -0,0 +1,21 @@
+import { DataSource, Repository} from "typeorm";
+import { Task } from "./entities/task.entity";
+import { Injectable } from "@nestjs/common";
+import { CreateTaskDto } from "./dto/create-task.dto";
+import { InjectRepository } from "@nestjs/typeorm";
+
+@Injectable()
+export class TasksRepository {
+    constructor(
+        @InjectRepository(Task)
+        private readonly taskRepository: Repository<Task>,
+      ) {}
+   
+    async findByName(id: number): Promise<Task>{
+        return await this.taskRepository.findOne({where: { id }})
+    }
+    async creatTask(createtask:CreateTaskDto) {
+        return await this.taskRepository.save(createtask);
+    }
+
+}
