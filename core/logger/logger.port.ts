b570d19d2605bd3d7d3dac5c91af2fdd3acaf255
CHENNAOUI Khadija <Khadija.CHENNAOUI@cegedim.com>
2023-11-14 11:18:41 +0100

    add Logger port

---
+++ core/logger/logger.port.ts
@@ -0,0 +1,5 @@
+export default interface LoggerPort {
+  info(message: string, context: string, metaData?: any): void;
+  warn(message: string, context: string, metaData?: any): void;
+  error(error: Error, context: string, metaData?: any): void;
+}
